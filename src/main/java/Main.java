0fd5ea0ae8840ec154abf179e0e387034061d91e
bingsuzoa <zxc_777@naver.com>
2025-09-30 11:34:39 +0900

    백준 디지털 시계?

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,55 +1,101 @@
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.*;
+import java.util.Arrays;
+import java.util.Scanner;
 
 class Main {
+    static int[] nums = new int[]{6, 2, 5, 5, 4, 5, 6, 3, 7, 5};
+    static long[][] dp;
+    static long number;
+    static int[] numbers;
+    static int N;
 
     public static void main(String[] args) throws IOException {
-        Main test = new Main();
 
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int n = Integer.parseInt(st.nextToken());
-        int m = Integer.parseInt(st.nextToken());
+        Scanner sc = new Scanner(System.in);
 
-        List<Integer>[] list = new ArrayList[n];
-        for(int i = 0; i < n; i++) {
-            list[i] = new ArrayList<>();
-            st = new StringTokenizer(br.readLine());
-            int k = Integer.parseInt(st.nextToken());
-            while(k --> 0) {
-                list[i].add(Integer.parseInt(st.nextToken()) -1);
+        String input = sc.next();
+        number = Long.parseLong(input);
+        numbers = Arrays.stream(input.split("")).mapToInt(Integer::parseInt).toArray();
+        dp = putDp();
+        N = input.length();
+
+        /// 일의 자리
+        long result = (long)Math.pow(10, N);
+        int prefix = nums[numbers[N -1]];
+        for(int d = 0; d < 10; d++) {
+            if(prefix == nums[d] && numbers[N -1] < d) {
+                result = Math.min(result, d - numbers[N-1]);
+            }
+            else if(prefix == nums[d] && numbers[N-1] >= d) {
+                result = Math.min(result, (long)Math.pow(10, N) - numbers[N-1] + d);
             }
         }
-        System.out.println(test.solution(list, n, m));
+        /// 십의 자리부터
+        int count = nums[numbers[N-1]];
+        for(int len = 2; len <= N; len ++) {
+            long digit = number % (long)Math.pow(10, len);
+            count += nums[numbers[N-len]];
+
+            for(int num = 0; num < 10; num++) {
+                if(count - nums[num] >= 0) {
+                    long pows = num * (long)Math.pow(10, len-1);
+                    long target = dp[len-1][count - nums[num]];
+                    if(target != Long.MAX_VALUE && digit != (pows + target)) {
+                        long value = pows + target - digit;
+                        if(value <= 0) {
+                            value += (long)Math.pow(10, N);
+                        }
+                        result = Math.min(result, value);
+                    }
+                }
+            }
+        }
+        System.out.println(result);
     }
 
-    private int solution(List<Integer>[] list, int n, int m) {
-        int count = 0;
-        int[] cows = new int[n];
-        int[] houses = new int[m];
-        Arrays.fill(cows, -1);
-        Arrays.fill(houses, -1);
-        for(int i = 0; i < n; i++) {
-            if(dfs(list, i, cows, houses, new boolean[houses.length])) {
-                count++;
+    private static long[][] putDp() {
+        long[][] dp = new long[15 + 1][15 * 7 + 1];
+
+        for(int i = 0; i < dp.length; i++) {
+            Arrays.fill(dp[i], Long.MAX_VALUE);
+        }
+
+        for(int d = 0; d <= 9; d++) {
+            dp[1][nums[d]] = Math.min(dp[1][nums[d]], d);
+        }
+
+        for(int len = 2; len < dp.length; len++) {
+            for(int sum = 0; sum < dp[len].length; sum++) {
+                for(int d = 0; d <= 9; d++) {
+                    if(sum - nums[d] < 0 || dp[len -1][sum - nums[d]] == Long.MAX_VALUE) continue;
+                    long newNum = (long)(d * Math.pow(10, len -1)) + dp[len-1][sum-nums[d]];
+                    dp[len][sum] = Math.min(dp[len][sum], newNum);
+                }
             }
         }
-        return count;
+        return dp;
     }
 
-    private boolean dfs(List<Integer>[] list, int cur, int[] cows, int[] houses, boolean[] checked) {
-        for(int num : list[cur]) {
-            if(checked[num]) continue;
-            checked[num] = true;
-            if(houses[num] == -1 || dfs(list, houses[num], cows, houses, checked)) {
-                cows[cur] = num;
-                houses[num] = cur;
-                return true;
-            }
+    private static long getNumber(int len) {
+        int startIdx = numbers.length - len;
+
+        int digit = len;
+        long number = 0;
+        for(int i = startIdx; i < numbers.length; i++) {
+            number += numbers[i] * (long)Math.pow(10, digit-1);
+            digit--;
         }
-        return false;
+        return number;
+    }
+
+    private static int getSum(int len) {
+        int sum = 0;
+        int startIdx = numbers.length - len;
+
+        for(int i = startIdx; i < numbers.length; i++) {
+            sum += nums[numbers[i]];
+        }
+        return sum;
     }
 
 
