bf5ad9681062330ccfb0349d86bb5d6dbd7c33b9
bingsuzoa <zxc_777@naver.com>
2025-10-05 13:25:03 +0900

    1026 보물

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,91 +1,47 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
-class Main {
-    static int N, M;
-    static int[][] graph;
-    static long answer;
+class Main { ;
 
     public static void main(String[] args) throws IOException {
-        answer = -1L;
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
+        int N = Integer.parseInt(br.readLine());
 
-        graph = new int[N][M];
+        int[] A = new int[N];
+        int[] B = new int[N];
 
-        for(int i = 0; i < graph.length; i++) {
-            String input = br.readLine();
-            for(int j = 0; j < graph[i].length; j++) {
-                graph[i][j] = input.charAt(j) - '0';
-            }
+        StringTokenizer Ainput = new StringTokenizer(br.readLine());
+        for(int i =0; i < A.length; i++) {
+            A[i] = Integer.parseInt(Ainput.nextToken());
+        }
+        Arrays.sort(A);
+
+        StringTokenizer Binput = new StringTokenizer(br.readLine());
+        for(int i = 0; i < B.length; i++) {
+            B[i] = Integer.parseInt(Binput.nextToken());
+        }
+        Arrays.sort(B);
+
+        int[] temp = new int[N];
+        for(int i =0 ; i< temp.length; i++) {
+            temp[i] = B[N-1-i];
         }
 
-        for(int idst = -N; idst < N; idst++) {
-            for(int jdst = -M; jdst < M; jdst++) {
-                for(int i = 0; i < graph.length; i++) {
-                    for(int j = 0; j < graph[i].length; j++) {
-                        bfs(idst, jdst, i, j);
-                    }
-                }
-            }
+
+        int sum = 0;
+        for(int i = 0; i < N; i++) {
+            sum += (A[i] * temp[i]);
         }
-        System.out.println(answer);
-    }
-    private static void bfs(int idst, int jdst, int x, int y) {
-        if(idst == 0 && jdst == 0) {
-            StringBuffer sb = new StringBuffer();
-            sb.append(graph[x][y]);
-            updateAnswer(sb.toString());
-            return;
-        }
-        if(idst != 0 && jdst == 0) {
-            StringBuffer sb = new StringBuffer();
-            for(int i = x; i < graph.length; i+= idst) {
-                if(i < 0) {
-                    updateAnswer(sb.toString());
-                    return;
-                }
-                sb.append(graph[i][y]);
-                updateAnswer(sb.toString());
-            }
-            return;
-        }
-        if(idst == 0 && jdst != 0) {
-            StringBuffer sb = new StringBuffer();
-            for(int j = y; j < graph[0].length; j+=jdst) {
-                if(j < 0) {
-                    updateAnswer(sb.toString());
-                    return;
-                }
-                sb.append(graph[x][j]);
-                updateAnswer(sb.toString());
-            }
-            return;
-        }
-        StringBuffer sb = new StringBuffer();
-        while(x >= 0 && y >= 0 && x < graph.length && y < graph[0].length) {
-            sb.append(graph[x][y]);
-            updateAnswer(sb.toString());
-            x += idst;
-            y += jdst;
-        }
+        System.out.println(sum);
+
+
+
     }
 
 
-    private static void updateAnswer(String s) {
-        s.replace(" ", "");
-        if(s.equals("")) {
-            return;
-        }
-        long num = Long.parseLong(s);
-        long compare = (long)Math.sqrt(num);
-        if(compare * compare == num) {
-            answer = Math.max(answer, num);
-        }
-    }
 }
