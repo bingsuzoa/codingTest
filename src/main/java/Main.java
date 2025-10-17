f9ba41318fe063b84eef9fb62eeb468894cdbfb9
bingsuzoa <zxc_777@naver.com>
2025-10-17 14:20:30 +0900

    물병

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,69 +2,47 @@ import java.util.*;
 import java.io.*;
 
 class Main {
-    static int N;
-    static int[][] original;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-        N = Integer.parseInt(st.nextToken());
-        long B = Long.parseLong(st.nextToken());
 
-        original = new int[N][N];
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
 
-        for(int i = 0; i < original.length; i++) {
-            StringTokenizer input = new StringTokenizer(br.readLine(), " ");
-            for(int j = 0; j < original[i].length; j++) {
-                original[i][j] = Integer.parseInt(input.nextToken());
+        String bit = Integer.toBinaryString(N);
+
+        int count = Integer.bitCount(N);
+
+        if(count <= K) {
+            System.out.println(0);
+            return;
+        }
+
+        int index = 0;
+        for(int i = 0; i < bit.length(); i++) {
+            if(K == 1) {
+                index = i;
+                break;
+            }
+            if(bit.charAt(i) == '1') {
+                K--;
             }
         }
 
-        int[][] result = pow(original, B);
+        String split = bit.substring(index);
+        int start = Integer.parseInt(split, 2);
 
-        StringBuffer sb = new StringBuffer();
-        for(int i = 0; i < result.length; i++) {
-            for(int j = 0; j < result[i].length; j++) {
-                sb.append(result[i][j] % 1000).append(" ");
+
+        int cur = start;
+        while(true) {
+            cur++;
+            int c = Integer.bitCount(cur);
+            if(c == 1) {
+                System.out.println(cur - start);
+                break;
             }
-            sb.append("\n");
         }
-        System.out.println(sb);
     }
 
-    private static int[][] pow(int[][] graph, long B) {
-        if(B == 1L) {
-            int[][] base = new int[N][N];
-            for(int i =0 ; i < graph.length; i++) {
-                for(int j = 0; j < graph[i].length; j++) {
-                    base[i][j] = graph[i][j];
-                }
-            }
-            return base;
-        }
-
-        int[][] ret = pow(graph, B/2);
-        int[][] multi = multiply(ret, ret);
-
-        if(B % 2 != 0) {
-            return multiply(multi, original);
-        }
-
-        return multi;
-    }
-
-    private static int[][] multiply(int[][] o1, int[][] o2) {
-        int[][] result = new int[N][N];
-
-        for(int i = 0; i < result.length; i++) {
-            for(int j = 0; j < result[i].length; j++) {
-                int sum = 0;
-                for(int k = 0; k < N; k++) {
-                    sum += (o1[i][k] * o2[k][j]) % 1000;
-                }
-                result[i][j] = sum % 1000;
-            }
-        }
-        return result;
-    }
 }
