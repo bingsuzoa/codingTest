3579a3879f23259b9c3854840aece252ab3f762b
bingsuzoa <zxc_777@naver.com>
2025-10-24 12:59:47 +0900

    차트

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,35 +1,56 @@
+import java.util.*;
 import java.io.*;
 
 class Main {
+    static int N;
+    static int[] graph;
+    static int[] order;
+    static int max = 0;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int N = Integer.parseInt(br.readLine());
-        double[] graph = new double[N];
+        N = Integer.parseInt(br.readLine());
+        graph = new int[N];
+
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        for(int i = 0; i < graph.length; i++) {
+            graph[i] = Integer.parseInt(st.nextToken());
+        }
+
+        order = new int[N];
+        perm(0,new boolean[N]);
+        System.out.println(max);
+    }
+
+    private static void perm(int cnt, boolean[] visited) {
+        if(order.length == cnt) {
+            check();
+            return;
+        }
 
         for(int i = 0; i < graph.length; i++) {
-            graph[i] = Double.parseDouble(br.readLine());
+            if(visited[i]) continue;
+            visited[i] = true;
+            order[cnt] = graph[i];
+            perm(cnt + 1, visited);
+            visited[i] = false;
         }
-
-        int min = 1000;
-        for(int n = 1; n <= 1000; n++) {
-            boolean isPossible = true;
-            for(double value : graph) {
-                double left = (value) * n;
-                double right = (value + 0.001) * n;
-
-                int leftInt = (int)Math.ceil(left - 1e-9);
-                int rightInt = (int)Math.floor(right - 1e-9);
-                if(leftInt > rightInt) {
-                    isPossible = false;
-                    break;
-                }
-            }
-            if(isPossible) {
-                min = Math.min(min, n);
-            }
-        }
-        System.out.println(min);
     }
+
+    private static void check() {
+        int[] prefix = new int[graph.length * 2 + 1];
+        for(int i = 1; i < prefix.length; i++) {
+            prefix[i] = prefix[i-1] + order[(i - 1) % N];
+        }
+
+        int count = 0;
+        for(int i = 0; i < N; i++) {
+            for(int j = i + 1; j <= i + N; j++) {
+                if(prefix[j] - prefix[i] == 50)count++;
+            }
+        }
+        max = Math.max(count/2, max);
+    }
+
 }
