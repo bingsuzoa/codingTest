48c783129bcfa8d7a17035428df6ac85a8380bdb
bingsuzoa <zxc_777@naver.com>
2025-10-25 14:57:28 +0900

    머리톡톡

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,55 +2,38 @@ import java.util.*;
 import java.io.*;
 
 class Main {
-    static int N;
-    static int[] graph;
-    static int[] order;
-    static int max = 0;
+    static int[] dp;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        N = Integer.parseInt(br.readLine());
-        graph = new int[N];
+        int N = Integer.parseInt(br.readLine());
 
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        int[] graph = new int[N];
+        dp = new int[1000001];
         for(int i = 0; i < graph.length; i++) {
-            graph[i] = Integer.parseInt(st.nextToken());
+            graph[i] = Integer.parseInt(br.readLine());
+            dp[graph[i]]++;
         }
-
-        order = new int[N];
-        perm(0,new boolean[N]);
-        System.out.println(max);
-    }
-
-    private static void perm(int cnt, boolean[] visited) {
-        if(order.length == cnt) {
-            check();
-            return;
-        }
-
+        StringBuilder sb = new StringBuilder();
         for(int i = 0; i < graph.length; i++) {
-            if(visited[i]) continue;
-            visited[i] = true;
-            order[cnt] = graph[i];
-            perm(cnt + 1, visited);
-            visited[i] = false;
+            sb.append(check(graph[i])).append("\n");
         }
+        System.out.println(sb);
     }
-
-    private static void check() {
-        int[] prefix = new int[graph.length * 2 + 1];
-        for(int i = 1; i < prefix.length; i++) {
-            prefix[i] = prefix[i-1] + order[(i - 1) % N];
-        }
-
+    private static int check(int num) {
         int count = 0;
-        for(int i = 0; i < N; i++) {
-            for(int j = i + 1; j <= i + N; j++) {
-                if(prefix[j] - prefix[i] == 50)count++;
+        for(int n = 1; n <= (int)Math.sqrt(num); n++) {
+            if(num % n == 0) {
+                int m = num / n;
+                if(n == m) {
+                    count += dp[n];
+                } else {
+                    count += dp[n];
+                    count += dp[m];
+                }
             }
         }
-        max = Math.max(count/2, max);
+        return count - 1;
     }
-
 }
