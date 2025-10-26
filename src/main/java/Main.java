cfdb31289002dd2362f8229cb69d1cf935cb535c
bingsuzoa <zxc_777@naver.com>
2025-10-26 20:03:19 +0900

    농장관리

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,38 +2,97 @@ import java.util.*;
 import java.io.*;
 
 class Main {
-    static int[] dp;
+    static int[][] graph;
+    static int N, M;
+    static int[] dx = {1,-1,0,0, 1, 1, -1, -1};
+    static int[] dy = {0,0,1,-1, 1, -1, -1, 1};
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int N = Integer.parseInt(br.readLine());
+        StringTokenizer st1 = new StringTokenizer(br.readLine(), " ");
+        N = Integer.parseInt(st1.nextToken());
+        M = Integer.parseInt(st1.nextToken());
 
-        int[] graph = new int[N];
-        dp = new int[1000001];
-        for(int i = 0; i < graph.length; i++) {
-            graph[i] = Integer.parseInt(br.readLine());
-            dp[graph[i]]++;
+        graph = new int[N][M];
+        int max = 0;
+        for(int i =0 ; i < graph.length; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+            for(int j = 0; j < graph[i].length; j++) {
+                graph[i][j] = Integer.parseInt(st.nextToken());
+                max = Math.max(max, graph[i][j]);
+            }
         }
-        StringBuilder sb = new StringBuilder();
-        for(int i = 0; i < graph.length; i++) {
-            sb.append(check(graph[i])).append("\n");
-        }
-        System.out.println(sb);
-    }
-    private static int check(int num) {
+
         int count = 0;
-        for(int n = 1; n <= (int)Math.sqrt(num); n++) {
-            if(num % n == 0) {
-                int m = num / n;
-                if(n == m) {
-                    count += dp[n];
-                } else {
-                    count += dp[n];
-                    count += dp[m];
+        boolean[][] checked = new boolean[N][M];
+        for(int n = max; n >= 1; n--) {
+            for(int i = 0; i < graph.length; i++) {
+                for(int j =0 ; j < graph[i].length; j++) {
+                    if(graph[i][j] == n && !checked[i][j]) {
+                        if(bfs(i, j, n)) {
+                            check(i,j,n, checked);
+                            count++;
+                        }
+                    }
                 }
             }
         }
-        return count - 1;
+        System.out.println(count);
     }
+    private static void check(int sx, int sy, int n, boolean[][] checked) {
+        Queue<int[]> queue = new LinkedList<>();
+        queue.add(new int[]{sx,sy});
+
+        while (!queue.isEmpty()) {
+            int[] cur = queue.poll();
+            int x = cur[0];
+            int y = cur[1];
+
+            checked[x][y] = true;
+
+            for(int i =0 ; i < 8; i++) {
+                int nx = x + dx[i];
+                int ny = y + dy[i];
+                if(nx >= N || nx < 0 || ny >= M || ny < 0) continue;
+                if(graph[nx][ny] == n && !checked[nx][ny]) {
+                    queue.add(new int[]{nx, ny});
+                }
+            }
+        }
+    }
+
+    private static boolean bfs(int sx, int sy, int n) {
+        Queue<int[]> queue = new LinkedList<>();
+        queue.add(new int[]{sx, sy});
+        boolean[][] visited = new boolean[N][M];
+        visited[sx][sy] = true;
+
+        while(!queue.isEmpty()) {
+            int[] cur = queue.poll();
+            int x = cur[0];
+            int y = cur[1];
+
+            if (graph[x][y] > n) {
+                return false;
+            }
+
+            if (graph[x][y] < n) {
+                continue;
+            }
+
+            for (int i = 0; i < 8; i++) {
+                int nx = x + dx[i];
+                int ny = y + dy[i];
+
+                if (nx >= N || nx < 0 || ny >= M || ny < 0 || visited[nx][ny]) {
+                    continue;
+                }
+                visited[nx][ny] = true;
+                queue.add(new int[]{nx, ny});
+            }
+        }
+        return true;
+    }
+
 }
