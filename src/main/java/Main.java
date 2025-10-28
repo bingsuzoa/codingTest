25f09bd29e7f82144ffab299b801ab4a4848f391
bingsuzoa <zxc_777@naver.com>
2025-10-28 12:37:58 +0900

    부분직사각형

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,52 +2,53 @@ import java.util.*;
 import java.io.*;
 
 class Main {
+    static int N,M;
+    static char[][] graph;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int N = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        graph = new char[N * 2][M * 2];
 
-        Info[] infos = new Info[N];
-        for(int i =0 ; i < infos.length; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-            int input_dur = Integer.parseInt(st.nextToken());
-            int input_end =  Integer.parseInt(st.nextToken());
-            infos[i] = new Info(i, input_dur, input_end);
-        }
-        Arrays.sort(infos);
-
-        int now = infos[0].end;
-        for(Info info : infos) {
-            int dur = info.dur;
-            int end = info.end;
-            if(end < now) {
-                now = (end - dur);
-            } else {
-                now -= dur;
+        for(int i = 0; i < N; i++) {
+            String input = br.readLine();
+            for(int j = 0; j < graph[i].length; j++) {
+                graph[i][j] = input.charAt(j % M);
             }
         }
-        if(now < 0) {
-            System.out.println(-1);
-            return;
+
+        for(int i = N; i < graph.length; i++) {
+            for(int j = 0; j < graph[i].length; j++) {
+                graph[i][j] = graph[i-N][j];
+            }
         }
-        System.out.println(now);
-    }
-}
 
-class Info implements Comparable<Info> {
-    int order;
-    int dur;
-    int end;
+        long[] answer = new long[26];
+        Map<Character, Integer> map = new HashMap<>();
+        int idx = 0;
+        for(char i = 'A'; i <= 'Z'; i++) {
+            map.put(i, idx++);
+        }
 
-    Info(int order, int dur, int end) {
-        this.order = order;
-        this.dur = dur;
-        this.end = end;
-    }
+        int realN = graph.length;
+        int realM = graph[0].length;
+        for(int i = 0; i < graph.length; i++) {
+            for(int j = 0; j < graph[i].length; j++) {
+                char c = graph[i][j];
+                int cIdx = map.get(c);
 
-    @Override
-    public int compareTo(Info o) {
-        return o.end - this.end;
+                long count = (i + 1) * (j + 1) * (realN - i) * (realM - j);
+                answer[cIdx] += count;
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for(long value : answer) {
+            sb.append(value).append("\n");
+        }
+        System.out.println(sb);
     }
 }
