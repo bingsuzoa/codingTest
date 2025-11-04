3f584e981317970e43d8357eedb94f23cb276d13
bingsuzoa <zxc_777@naver.com>
2025-11-04 23:18:56 +0900

    강의실

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,63 +2,45 @@ import java.util.*;
 import java.io.*;
 
 class Main {
-    static List<Node> results;
-    
+    static List<int[]> list;
+    static PriorityQueue<Integer> pq;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int N = Integer.parseInt(br.readLine());
+        list = new ArrayList<>();
+        pq = new PriorityQueue<>((o1,o2) -> {
+            return o1 - o2;
+        });
 
-        results = new ArrayList<>();
-        results.add(new Node(0, ""));
-        while(N > 0) {
+        while(N --> 0) {
             String input = br.readLine();
-            if (input == null || input.isEmpty()) continue;
-            String[] array = input.split(" ");
-            if(array[0].contains("type")) {
-                String value = array[1].trim();
-                int second = Integer.parseInt(array[2].trim());
-
-                String lastValue = results.get(results.size() -1).value;
-                results.add(new Node(second, lastValue + value));
-
-            }
-            else if(array[0].contains("undo")) {
-                int to = Integer.parseInt(array[1].trim());
-                int from = Integer.parseInt(array[2].trim());
-                int idx = results.size() - 1;
-                int goal = from - to - 1;
-
-                boolean isFlag = false;
-                while(idx >= 0) {
-                    Node cur = results.get(idx);
-                    int sec = cur.sec;
-                    String value = cur.value;
-                    if(goal < sec) {
-                        idx--;
-                    } else {
-                        results.add(new Node(from, value));
-                        isFlag = true;
-                        break;
-                    }
-                }
-                if(!isFlag) {
-                    results.add(new Node(from, ""));
-                }
-            }
-            N--;
+            String[] temp = input.split(" ");
+            int[] array = new int[3];
+            array[0] = Integer.parseInt(temp[0].trim());
+            array[1] = Integer.parseInt(temp[1].trim());
+            array[2] = Integer.parseInt(temp[2].trim());
+            list.add(array);
         }
-        System.out.println(results.get(results.size() - 1).value);
-    }
-}
 
-class Node {
-    int sec;
-    String value;
+        Collections.sort(list, (o1, o2) -> {
+            if (o1[1] == o2[1]) return o1[2] - o2[2];
+            return o1[1] - o2[1];
+        });
 
-    public Node(int sec, String value) {
-        this.sec = sec;
-        this.value = value;
+
+        int max = 0;
+        for(int[] next : list) {
+            int start = next[1];
+            int end = next[2];
+
+            while(!pq.isEmpty() && pq.peek() <= start) {
+                pq.poll();
+            }
+            pq.add(end);
+            max = Math.max(max, pq.size());
+        }
+        System.out.println(max);
     }
 }
