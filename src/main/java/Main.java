b736548d8b2fdcff8727dc929b788ac3a5930ca7
bingsuzoa <zxc_777@naver.com>
2025-11-06 19:30:06 +0900

    암호

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,45 +2,25 @@ import java.util.*;
 import java.io.*;
 
 class Main {
-    static List<int[]> list;
-    static PriorityQueue<Integer> pq;
+    static int MOD = 900528;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int N = Integer.parseInt(br.readLine());
-        list = new ArrayList<>();
-        pq = new PriorityQueue<>((o1,o2) -> {
-            return o1 - o2;
-        });
+        String input = br.readLine();
+        String goal = br.readLine();
 
-        while(N --> 0) {
-            String input = br.readLine();
-            String[] temp = input.split(" ");
-            int[] array = new int[3];
-            array[0] = Integer.parseInt(temp[0].trim());
-            array[1] = Integer.parseInt(temp[1].trim());
-            array[2] = Integer.parseInt(temp[2].trim());
-            list.add(array);
+        long answer = 0;
+
+        Map<Character, Integer> map = new HashMap<>();
+        for(int i = 0; i < input.length(); i++) {
+            map.put(input.charAt(i), i + 1);
         }
 
-        Collections.sort(list, (o1, o2) -> {
-            if (o1[1] == o2[1]) return o1[2] - o2[2];
-            return o1[1] - o2[1];
-        });
-
-
-        int max = 0;
-        for(int[] next : list) {
-            int start = next[1];
-            int end = next[2];
-
-            while(!pq.isEmpty() && pq.peek() <= start) {
-                pq.poll();
-            }
-            pq.add(end);
-            max = Math.max(max, pq.size());
+        int base = input.length();
+        for(int i = 0; i < goal.length(); i++) {
+            answer = (answer * base + map.get(goal.charAt(i))) % MOD;
         }
-        System.out.println(max);
+        System.out.println(answer % MOD);
     }
-}
+    }
