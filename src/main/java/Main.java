ea51df03caba37055949bce0244bcc8d2fabbaa3
bingsuzoa <zxc_777@naver.com>
2025-11-13 11:32:02 +0900

    영어읽기

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,63 +2,67 @@ import java.util.*;
 import java.io.*;
 
 class Main {
-    static int[] graph;
-    static int[] result;
-    static boolean found;
+    static Map<String, Integer> map;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int N = Integer.parseInt(br.readLine());
-        graph = new int[N];
-        result = new int[N * 2];
-        found = false;
+        int n = Integer.parseInt(br.readLine());
+        map = new HashMap<>();
+        while(n --> 0) {
+            String input = br.readLine().toLowerCase();
+            char[] charArray = input.toCharArray();
 
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-        for(int i = 0; i < N; i++) {
-            graph[i] = Integer.parseInt(st.nextToken());
+            if(input.length() <= 2) {
+                map.put(input, map.getOrDefault(input, 0) + 1);
+                continue;
+            }
+
+            String temp = input.substring(1, input.length() -1);
+            char[] tempArray = temp.toCharArray();
+            Arrays.sort(tempArray);
+            int idx = 1;
+            for(char c : tempArray) {
+                charArray[idx++] = c;
+            }
+
+            String newInput = new String(charArray);
+            map.put(newInput, map.getOrDefault(newInput, 0) + 1);
         }
-        Arrays.sort(graph);
-        Arrays.fill(result, -1);
-        dfs(new boolean[N], 0);
 
-        if(!found) {
-            System.out.println(-1);
+
+        int m = Integer.parseInt(br.readLine());
+        while(m --> 0) {
+            int count = 1;
+            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+            while(st.hasMoreTokens()) {
+                String input = st.nextToken().toLowerCase();
+                count *= getResult(input);
+            }
+            System.out.println(count);
         }
     }
-
-    private static void dfs(boolean[] visited, int cur) {
-        if(found) {
-            return;
-        }
-
-        if(cur == result.length) {
-            found = true;
-            StringBuilder sb = new StringBuilder();
-            for(int i = 0; i < result.length; i++) {
-                sb.append(result[i]).append(" ");
+    private static int getResult(String word) {
+        if(word.length() <= 2) {
+            if(!map.containsKey(word)) {
+                return 0;
             }
-            System.out.println(sb);
-            return;
-        }
-        if(result[cur] != -1) {
-            dfs(visited, cur + 1);
-            return;
+            return map.get(word);
         }
 
-        for(int i = 0; i < graph.length; i++) {
-            if(!visited[i]) {
-                int pair = cur + graph[i] + 1;
-                if(pair >= result.length || result[pair] != -1) {
-                    continue;
-                }
-                visited[i] = true;
-                result[cur] = result[pair] = graph[i];
-                dfs(visited, cur + 1);
-                result[cur] = result[pair] = -1;
-                visited[i] = false;
-                if(found) return;
-            }
+        char[] wordArray = word.toCharArray();
+        String temp= word.substring(1, word.length() -1);
+        char[] tempArray = temp.toCharArray();
+        Arrays.sort(tempArray);
+        int idx = 1;
+        for(char c : tempArray) {
+            wordArray[idx++] = c;
         }
+        String result = new String(wordArray);
+        if(!map.containsKey(result)) {
+            return 0;
+        }
+        return map.get(result);
     }
 }
