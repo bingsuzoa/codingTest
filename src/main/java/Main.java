45aed617b8d15e8298afd75441df1f1a5b5541c1
bingsuzoa <zxc_777@naver.com>
2025-11-14 09:01:11 +0900

    주기문으로 바꾸기

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,67 +2,47 @@ import java.util.*;
 import java.io.*;
 
 class Main {
-    static Map<String, Integer> map;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int n = Integer.parseInt(br.readLine());
-        map = new HashMap<>();
-        while(n --> 0) {
-            String input = br.readLine().toLowerCase();
-            char[] charArray = input.toCharArray();
+        int N = Integer.parseInt(br.readLine());
+        String input = br.readLine();
 
-            if(input.length() <= 2) {
-                map.put(input, map.getOrDefault(input, 0) + 1);
-                continue;
-            }
-
-            String temp = input.substring(1, input.length() -1);
-            char[] tempArray = temp.toCharArray();
-            Arrays.sort(tempArray);
-            int idx = 1;
-            for(char c : tempArray) {
-                charArray[idx++] = c;
-            }
-
-            String newInput = new String(charArray);
-            map.put(newInput, map.getOrDefault(newInput, 0) + 1);
+        int minCount = Integer.MAX_VALUE;
+        for(int i = N; i >= 1; i--) {
+            minCount = Math.min(minCount, countChange(input, i));
         }
-
-
-        int m = Integer.parseInt(br.readLine());
-        while(m --> 0) {
-            int count = 1;
-            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-
-            while(st.hasMoreTokens()) {
-                String input = st.nextToken().toLowerCase();
-                count *= getResult(input);
-            }
-            System.out.println(count);
-        }
+        System.out.println(minCount);
     }
-    private static int getResult(String word) {
-        if(word.length() <= 2) {
-            if(!map.containsKey(word)) {
-                return 0;
-            }
-            return map.get(word);
-        }
 
-        char[] wordArray = word.toCharArray();
-        String temp= word.substring(1, word.length() -1);
-        char[] tempArray = temp.toCharArray();
-        Arrays.sort(tempArray);
-        int idx = 1;
-        for(char c : tempArray) {
-            wordArray[idx++] = c;
+    private static int countChange(String input, int n) {
+        int count = 0;
+        for(int i = 0; i < n; i++) {
+            int A = 0;
+            int G = 0;
+            int C = 0;
+            int T = 0;
+            int min = Integer.MAX_VALUE;
+            for(int j = i; j < input.length(); j += n) {
+                char c = input.charAt(j);
+                if(c != 'A') {
+                    A++;
+                }
+                if(c != 'G') {
+                    G++;
+                }
+                if(c != 'C') {
+                    C++;
+                }
+                if(c != 'T') {
+                    T++;
+                }
+            }
+            min = Math.min(min, Math.min(A, G));
+            min = Math.min(min, Math.min(C, T));
+            count += min;
         }
-        String result = new String(wordArray);
-        if(!map.containsKey(result)) {
-            return 0;
-        }
-        return map.get(result);
+        return count;
     }
 }
