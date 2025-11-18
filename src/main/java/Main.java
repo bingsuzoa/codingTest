5b51b4aa47011c41880d7344b80487b062e50d30
bingsuzoa <zxc_777@naver.com>
2025-11-18 09:04:29 +0900

    게임

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,90 +1,96 @@
-import java.util.*;
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.PriorityQueue;
+import java.util.Queue;
+import java.util.StringTokenizer;
 
 class Main {
-    static int[][] goals = new int[8][7];
-    static boolean[][] visited = new boolean[8][7];
-    static boolean[][] dominos = new boolean[7][7];
-    static int answer = 0;
+    static int[][] graph = new int[501][501];
+    static boolean[][] visited = new boolean[501][501];
+    static int[] dx = {1, -1, 0, 0};
+    static int[] dy = {0, 0, 1, -1};
+    static int answer = Integer.MAX_VALUE;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        for(int i = 0; i < goals.length; i++) {
-            String input = br.readLine();
-            for(int j = 0; j < input.length(); j++) {
-                goals[i][j] = input.charAt(j) - '0';
+        int N = Integer.parseInt(br.readLine());
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+            int x1 = Integer.parseInt(st.nextToken());
+            int y1 = Integer.parseInt(st.nextToken());
+            int x2 = Integer.parseInt(st.nextToken());
+            int y2 = Integer.parseInt(st.nextToken());
+
+            for(int j = Math.min(x1, x2); j <= Math.max(x1, x2); j++) {
+                for(int k = Math.min(y1, y2); k <= Math.max(y1, y2); k++) {
+                    graph[j][k] = 1;
+                }
             }
         }
-        dfs(0,0);
+
+        int M = Integer.parseInt(br.readLine());
+        for (int i = 0; i < M; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+            int x1 = Integer.parseInt(st.nextToken());
+            int y1 = Integer.parseInt(st.nextToken());
+            int x2 = Integer.parseInt(st.nextToken());
+            int y2 = Integer.parseInt(st.nextToken());
+
+            for(int j = Math.min(x1, x2); j <= Math.max(x1, x2); j++) {
+                for(int k = Math.min(y1, y2); k <= Math.max(y1, y2); k++) {
+                    graph[j][k] = 2;
+                }
+            }
+        }
+        bfs();
+        if (answer == Integer.MAX_VALUE) {
+            System.out.println(-1);
+            return;
+        }
         System.out.println(answer);
     }
 
-    private static void dfs(int r, int c) {
-        if(r == 8) {
-            answer++;
-            return;
-        }
-        if(c == 7) {
-            dfs(r+1, 0);
-            return;
-        }
+    private static void bfs() {
+        PriorityQueue<int[]> queue = new PriorityQueue<>((o1,o2) -> {
+            return o1[2] - o2[2];
+        });
+        queue.add(new int[]{0, 0, 0});
+        visited[0][0] = true;
 
-        if(!visited[r][c]) {
-            visited[r][c] = true;
-            int curNum = goals[r][c];
+        while (!queue.isEmpty()) {
+            int[] cur = queue.poll();
+            int x = cur[0];
+            int y = cur[1];
+            int count = cur[2];
 
-            for(int i = 0; i < 2; i++) {
-                if(i == 0) {
-                    int nextR = r+1;
-                    int nextC = c;
+            if (x == 500 && y == 500) {
+                answer = Math.min(answer, count);
+                break;
+            }
 
-                    if(nextR >= 8 || nextC >= 7) {
-                        continue;
-                    }
+            for (int i = 0; i < 4; i++) {
+                int nx = x + dx[i];
+                int ny = y + dy[i];
 
-                    if(visited[nextR][nextC]) {
-                        continue;
-                    }
-
-                    int pairNum = goals[nextR][nextC];
-                    if(dominos[curNum][pairNum]) {
-                        continue;
-                    }
-
-                    visited[nextR][nextC] = true;
-                    dominos[curNum][pairNum] = true;
-                    dominos[pairNum][curNum] = true;
-                    dfs(r, c+1);
-                    visited[nextR][nextC] = false;
-                    dominos[curNum][pairNum] = false;
-                    dominos[pairNum][curNum] = false;
+                if (nx >= graph.length || nx < 0 || ny >= graph[0].length || ny < 0) {
+                    continue;
                 }
-                if(i == 1) {
-                    int nextR = r;
-                    int nextC = c+1;
-                    if(nextR >= 8 || nextC >= 7) {
-                        continue;
-                    }
-                    int pairNum = goals[nextR][nextC];
-                    if(visited[nextR][nextC]) {
-                        continue;
-                    }
-                    if(dominos[curNum][pairNum]) {
-                        continue;
-                    }
-                    visited[nextR][nextC] = true;
-                    dominos[curNum][pairNum] = true;
-                    dominos[pairNum][curNum] = true;
-                    dfs(nextR, nextC);
-                    visited[nextR][nextC] = false;
-                    dominos[curNum][pairNum] = false;
-                    dominos[pairNum][curNum] = false;
+                if (graph[nx][ny] == 2) {
+                    continue;
+                }
+                if(visited[nx][ny]) {
+                    continue;
+                }
+                visited[nx][ny] = true;
+                if (graph[nx][ny] == 1) {
+                    queue.add(new int[]{nx, ny, count + 1});
+                } else if (graph[nx][ny] == 0) {
+                    queue.add(new int[]{nx, ny, count});
                 }
             }
-            visited[r][c] = false;
-        } else {
-            dfs(r, c+1);
         }
     }
 }
