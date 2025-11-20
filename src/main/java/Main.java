de3cc03cc7e6557c2cbb59c19224c77789f2fd0b
bingsuzoa <zxc_777@naver.com>
2025-11-20 14:02:23 +0900

    멍멍이 쓰다듬기

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,40 +1,32 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
+import java.io.*;
 
 class Main {
-    static int[] graph;
-    static int[] prefix;
-    static int[] dp;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int N = Integer.parseInt(br.readLine());
-        graph = new int[125];
-        prefix = new int[125];
-        dp = new int[N + 1];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        long start = Integer.parseInt(st.nextToken());
+        long end = Integer.parseInt(st.nextToken());
 
-        for (int i = 1; i < graph.length; i++) {
-            graph[i] = graph[i - 1] + i;
-        }
-        prefix[1] = graph[1];
-        for (int i = 2; i < prefix.length; i++) {
-            prefix[i] = prefix[i - 1] + graph[i];
+        long goal = end - start;
+
+        if(goal == 0) {
+            System.out.println(0);
+            return;
         }
 
-        Arrays.fill(dp, 300000);
-        dp[1] = 1;
-        dp[0] = 0;
-        for(int i = 2; i < dp.length; i++) {
-            for(int j = 1; j < prefix.length; j++) {
-                if(i - prefix[j] < 0) continue;
-                dp[i] = Math.min(dp[i], dp[i - prefix[j]] + 1);
-            }
+        long k = (long)Math.sqrt(goal);
+
+        if(k * k == goal) {
+            System.out.println(2 * k - 1);
         }
-        System.out.println(dp[N]);
+        else if(k * k < goal && k * (k + 1) >= goal) {
+            System.out.println(2* k);
+        } else {
+            System.out.println(2 * k + 1);
+        }
+
     }
 }
