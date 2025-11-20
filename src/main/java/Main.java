d07b837520794496136aecd0e891c5b2e2baeaa1
bingsuzoa <zxc_777@naver.com>
2025-11-20 09:34:03 +0900

    캡틴 이다솜

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,74 +1,40 @@
-import java.util.*;
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 
 class Main {
-    static String[] board;
+    static int[] graph;
+    static int[] prefix;
+    static int[] dp;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int N = Integer.parseInt(br.readLine());
-        board = new String[N];
+        graph = new int[125];
+        prefix = new int[125];
+        dp = new int[N + 1];
 
-        for(int i = 0; i < board.length; i++) {
-            String input = br.readLine();
-            input = change(input, 'k', 'c');
-            input = changeString(input, 'g', 'z');
-            board[i] = input;
+        for (int i = 1; i < graph.length; i++) {
+            graph[i] = graph[i - 1] + i;
         }
-        Arrays.sort(board);
+        prefix[1] = graph[1];
+        for (int i = 2; i < prefix.length; i++) {
+            prefix[i] = prefix[i - 1] + graph[i];
+        }
 
-        for(int i = 0; i < board.length; i++) {
-            String input = board[i];
-            input = change(input, 'c', 'k');
-            input = changeString(input, 'z', 'g');
-            System.out.println(input);
-        }
-    }
-    private static String changeString(String input, char from, char to) {
-        char[] array = new char[input.length()];
-        for(int i = 0; i < input.length(); i++) {
-            char c = input.charAt(i);
-            if(c == 'n') {
-                if(i + 1 < input.length()) {
-                    char p = input.charAt(i+1);
-                    if(p == from) {
-                        array[i] = 'n';
-                        array[i+1] = to;
-                        i = i+1;
-                    } else {
-                        array[i] = 'n';
-                        array[i+1] = p;
-                        i = i+1;
-                    }
-                } else {
-                    array[i] = 'n';
-                }
-            } else {
-                array[i] = c;
+        Arrays.fill(dp, 300000);
+        dp[1] = 1;
+        dp[0] = 0;
+        for(int i = 2; i < dp.length; i++) {
+            for(int j = 1; j < prefix.length; j++) {
+                if(i - prefix[j] < 0) continue;
+                dp[i] = Math.min(dp[i], dp[i - prefix[j]] + 1);
             }
         }
-        StringBuilder sb = new StringBuilder();
-        for(char c : array) {
-            sb.append(c);
-        }
-        return sb.toString();
-    }
-
-    private static String change(String input, char from, char to) {
-        char[] array = new char[input.length()];
-        for(int i = 0; i < input.length(); i++) {
-            char c = input.charAt(i);
-            if(c == from) {
-                array[i] = to;
-            } else {
-                array[i] = c;
-            }
-        }
-        StringBuilder sb = new StringBuilder();
-        for(char c : array) {
-            sb.append(c);
-        }
-        return sb.toString();
+        System.out.println(dp[N]);
     }
 }
