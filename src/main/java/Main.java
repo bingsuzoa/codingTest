221bbb287f54b4e52cceb1181528aa5c25e26f85
bingsuzoa <zxc_777@naver.com>
2025-11-24 01:53:25 +0900

    토달기

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,44 +2,67 @@ import java.util.*;
 import java.io.*;
 
 class Main {
+    static List<String>[] list;
+    static String input;
+    static Queue<String> queue;
 
-    public static void main(String[] args) throws IOException{
+    static String answer;
+    static int max = 0;
+
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int T = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        input = st.nextToken();
 
-        while(T --> 0)  {
-            String input = br.readLine();
+        list = new ArrayList[81];
+        for(int i =0 ; i < list.length; i++) {
+            list[i] = new ArrayList<>();
+        }
+        for(int i = 0; i < N; i++) {
+            String s = br.readLine();
+            list[s.length()].add(s);
+        }
 
-            if(input.length() == 1) {
-                System.out.println("YES");
-                continue;
+        queue = new LinkedList<>();
+        queue.add(input);
+
+        while(!queue.isEmpty()) {
+            String cur = queue.poll();
+
+            for(String next : list[cur.length() + 1]) {
+                int left = 0;
+                int right = 0;
+                int oppt = 0;
+
+                while(left < cur.length() && right < next.length()) {
+                    if(cur.charAt(left) != next.charAt(right)) {
+                        if(oppt < 1) {
+                            oppt++;
+                            right++;
+                        } else {
+                            break;
+                        }
+                    } else {
+                        left ++;
+                        right ++;
+                    }
+                }
+                if(left == cur.length() && oppt <= 1) {
+                    if(max < next.length()) {
+                        max = next.length();
+                        answer = next;
+                    }
+                    queue.add(next);
+                }
             }
+        }
 
-            if(isPass(input, 0, input.length() - 1)) {
-                System.out.println("YES");
-            } else {
-                System.out.println("NO");
-            }
+        if(max == 0) {
+            System.out.println(input);
+        } else {
+            System.out.println(answer);
         }
     }
-
-    private static boolean isPass(String input, int left, int right) {
-        int mid = (left + right) / 2;
-        if(right - left >= 3) {
-            if(!isPass(input, left, mid -1)) {
-                return false;
-            }
-            if(!isPass(input, mid + 1, right)) {
-                return false;
-            }
-        }
-
-        for(int i = left, j = right; i < mid; i++, j--) {
-            if(input.charAt(i) == input.charAt(j)) {
-                return false;
-            }
-        }
-        return true;
-    }
-}   
+}
