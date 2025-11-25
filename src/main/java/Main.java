4b8b53b16add247d24c8fc9a95d7daf8e70b7f88
bingsuzoa <zxc_777@naver.com>
2025-11-25 14:11:16 +0900

    소수인 팰린드롬

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,57 +2,50 @@ import java.util.*;
 import java.io.*;
 
 class Main {
-    static List<int[]>[] list;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        int N = Integer.parseInt(br.readLine());
-        int M = Integer.parseInt(br.readLine());
-
-        list = new ArrayList[N+1];
-        for(int i = 0; i < list.length; i++) {
-            list[i] = new ArrayList<>();
-        }
-
-        while(M --> 0) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            int s = Integer.parseInt(st.nextToken());
-            int e = Integer.parseInt(st.nextToken());
-            int c = Integer.parseInt(st.nextToken());
-            list[s].add(new int[]{e, c});
-        }
 
         StringTokenizer st = new StringTokenizer(br.readLine());
         int start = Integer.parseInt(st.nextToken());
         int end = Integer.parseInt(st.nextToken());
 
-        PriorityQueue<int[]> pq = new PriorityQueue<>((o1,o2) -> {
-            return o1[1] - o2[1];
-        });
+        boolean[] graph = new boolean[end + 1];
+        Arrays.fill(graph, true);
+        graph[0] = false;
+        graph[1] = false;
 
-        int[] costs = new int[N+1];
-        Arrays.fill(costs, Integer.MAX_VALUE);
 
-        pq.add(new int[]{start, 0});
-        costs[0] = 0;
-
-        while(!pq.isEmpty()) {
-            int[] from = pq.poll();
-            int cost = from[1];
-
-            if(costs[from[0]] < cost) {
-                continue;
-            }
-
-            for(int[] to : list[from[0]]) {
-                int next = to[0];
-                if(costs[next] > cost + to[1]) {
-                    costs[next] = cost + to[1];
-                    pq.add(new int[]{next, costs[next]});
+        for(int i = 2; i * i < graph.length; i++) {
+            if(graph[i]) {
+                for(int j = i + i; j < graph.length; j+= i) {
+                    graph[j] = false;
                 }
             }
         }
-        System.out.println(costs[end]);
+
+        StringBuilder sb = new StringBuilder();
+        for(int i = start; i <= end; i++) {
+            if(graph[i] && isTrue(i)) {
+                sb.append(i).append("\n");
+            }
+        }
+        System.out.println(sb + "-1");
     }
+
+    private static boolean isTrue(int num) {
+        String n = String.valueOf(num);
+        int[] temp = new int[n.length()];
+        for(int i = 0; i < temp.length; i++) {
+            temp[i] = n.charAt(i) - '0';
+        }
+
+        for(int i = 0; i < temp.length / 2; i++) {
+            if(temp[i] != temp[temp.length - 1 -i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
 }
