940bef993ccead01391cac790ccc2fb7710ff3d8
bingsuzoa <zxc_777@naver.com>
2025-12-01 08:06:04 +0900

    내려가기

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,44 +2,46 @@ import java.io.*;
 import java.util.*;
 
 class Main {
-    static int N;
-    static StringBuilder sb;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        N = Integer.parseInt(br.readLine());
-        sb = new StringBuilder();
+        int N = Integer.parseInt(br.readLine());
 
-        for(int i = 2; i < 10; i++) {
-            if(isPrime(i)) {
-                dfs(i, 1);
-            }
+        int[][] dp = new int[2][3];
+        StringTokenizer first = new StringTokenizer(br.readLine());
+        for(int i = 0; i < 3; i++) {
+            dp[0][i] = Integer.parseInt(first.nextToken());
+            dp[1][i] = dp[0][i];
         }
-        System.out.println(sb.toString().trim());
-    }
 
-    private static void dfs(int num, int len) {
-        if(len == N) {
-            sb.append(num).append("\n");
-            return;
-        }
-        num *= 10;
-        for(int i = 0; i < 10; i++) {
-            int result = num + i;
-            if(isPrime(result)) {
-                dfs(result, len + 1);
-            }
-        }
-    }
+        for(int i = 1; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int n = Integer.parseInt(st.nextToken());
+            int m = Integer.parseInt(st.nextToken());
+            int k =  Integer.parseInt(st.nextToken());
 
-    private static boolean isPrime(int num) {
-        if(num < 2) return false;
-        for(int i = 2; i * i <= num; i++) {
-            if(num % i == 0) {
-                return false;
-            }
+            int nn = Math.max(dp[0][0], dp[0][1]) + n;
+            int mm = Math.max(dp[0][0], Math.max(dp[0][1], dp[0][2])) + m;
+            int kk = Math.max(dp[0][1], dp[0][2]) + k;
+            dp[0][0] = nn;
+            dp[0][1] = mm;
+            dp[0][2] = kk;
+
+            int nnn = Math.min(dp[1][0], dp[1][1]) + n;
+            int mmm = Math.min(dp[1][0], Math.min(dp[1][1], dp[1][2])) + m;
+            int kkk = Math.min(dp[1][1], dp[1][2]) + k;
+            dp[1][0] = nnn;
+            dp[1][1] = mmm;
+            dp[1][2] = kkk;
         }
-        return true;
+
+        int maxAns = 0;
+        int minAns = Integer.MAX_VALUE;
+        for(int i = 0; i < 3; i++) {
+            maxAns = Math.max(maxAns, dp[0][i]);
+            minAns = Math.min(minAns, dp[1][i]);
+        }
+        System.out.println(maxAns + " " + minAns);
     }
 }
