fa4850240153ab7f53c1107155eb29a35b665132
bingsuzoa <zxc_777@naver.com>
2025-12-02 07:39:45 +0900

    포함하는 구간

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,5 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
 class Main {
 
@@ -8,40 +8,78 @@ class Main {
 
         int N = Integer.parseInt(br.readLine());
 
-        int[][] dp = new int[2][3];
-        StringTokenizer first = new StringTokenizer(br.readLine());
-        for(int i = 0; i < 3; i++) {
-            dp[0][i] = Integer.parseInt(first.nextToken());
-            dp[1][i] = dp[0][i];
-        }
-
-        for(int i = 1; i < N; i++) {
+        List<Pair> pairs = new ArrayList<>();
+        List<Long> nums = new ArrayList<>();
+        for(int i = 0; i < N; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
-            int n = Integer.parseInt(st.nextToken());
-            int m = Integer.parseInt(st.nextToken());
-            int k =  Integer.parseInt(st.nextToken());
-
-            int nn = Math.max(dp[0][0], dp[0][1]) + n;
-            int mm = Math.max(dp[0][0], Math.max(dp[0][1], dp[0][2])) + m;
-            int kk = Math.max(dp[0][1], dp[0][2]) + k;
-            dp[0][0] = nn;
-            dp[0][1] = mm;
-            dp[0][2] = kk;
-
-            int nnn = Math.min(dp[1][0], dp[1][1]) + n;
-            int mmm = Math.min(dp[1][0], Math.min(dp[1][1], dp[1][2])) + m;
-            int kkk = Math.min(dp[1][1], dp[1][2]) + k;
-            dp[1][0] = nnn;
-            dp[1][1] = mmm;
-            dp[1][2] = kkk;
+            long x = Long.parseLong(st.nextToken());
+            long y = Long.parseLong(st.nextToken());
+            pairs.add(new Pair(x, y));
+            nums.add(x);
+            nums.add(y);
         }
 
-        int maxAns = 0;
-        int minAns = Integer.MAX_VALUE;
-        for(int i = 0; i < 3; i++) {
-            maxAns = Math.max(maxAns, dp[0][i]);
-            minAns = Math.min(minAns, dp[1][i]);
+        Collections.sort(nums);
+        Map<Long, Integer> map = new HashMap<>();
+        int idx = 0;
+        for(long num : nums) {
+            if(!map.containsKey(num)) {
+                map.put(num, idx++);
+            }
         }
-        System.out.println(maxAns + " " + minAns);
+
+        boolean[] start = new boolean[idx];
+        boolean[] end = new boolean[idx];
+        List<Pair> data = new ArrayList<>();
+        for(Pair pair : pairs) {
+            int x = map.get(pair.x);
+            int y = map.get(pair.y);
+            data.add(new Pair(x, y));
+            start[x] = true;
+            end[y] = true;
+        }
+
+        data.sort((o1,o2) -> {
+            return (int)((o1.y - o1.x) - (o2.y - o2.x));
+        });
+
+        int[] sdp = new int[idx];
+        int[] edp = new int[idx];
+
+        if(start[0]) sdp[0] = 1;
+        if(end[0]) edp[0] = 1;
+
+        for(int i = 1; i < idx; i++) {
+            sdp[i] = sdp[i-1] + (start[i] ? 1 : 0);
+            edp[i] = edp[i-1] + (end[i] ? 1 : 0);
+        }
+
+        int answer = 0;
+        for(int i = data.size() - 1; i >= 0; i--) {
+            Pair p = data.get(i);
+            int s = (int)p.x;
+            int e = (int)p.y;
+
+            int val1;
+            if(e > 0) {
+                val1 = edp[idx - 1] - edp[e -1];
+            } else {
+                val1 = edp[idx - 1];
+            }
+            int val2 = sdp[s];
+
+            answer = Math.max(answer, N - val1 - val2 + 1);
+        }
+        System.out.println(answer);
+    }
+}
+
+class Pair {
+    long x;
+    long y;
+
+    public Pair(long x, long y) {
+        this.x = x;
+        this.y = y;
     }
 }
