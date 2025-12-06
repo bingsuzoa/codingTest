eb645c440f4f23ca34afac92eeb5dcfe953d285c
bingsuzoa <zxc_777@naver.com>
2025-12-07 08:48:27 +0900

    다각형의 넓이

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,101 +2,41 @@ import java.util.*;
 import java.io.*;
 
 class Main {
-    static int M, N;
-    static char[][] graph;
-    static boolean[][][] visited;
-    static int answer = 0;
-    static int[] dx = {-1, 1, 0, 0};
-    static int[] dy = {0, 0, -1, 1};
+    static List<int[]> list;
+
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        StringTokenizer input = new StringTokenizer(br.readLine());
-        M = Integer.parseInt(input.nextToken());
-        N = Integer.parseInt(input.nextToken());
+        int N = Integer.parseInt(br.readLine());
+        list = new ArrayList<>();
 
-        graph = new char[M][N];
-        for(int i = 0; i < M; i++) {
-            String st = br.readLine();
-            for(int j = 0; j < N; j++) {
-                graph[i][j] = st.charAt(j);
-            }
+        for(int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+            list.add(new int[]{x, y});
         }
-        visited = new boolean[M][N][4];
-        checkLine();
-        checkVertical();
-        System.out.println(answer);
-    }
+        list.add(new int[]{list.get(0)[0], list.get(0)[1]});
 
-    private static void checkLine() {
-        for(int i = 0; i < M; i++) {
-            for(int j = 0; j < N - 1; j++) {
-                int lx = i;
-                int ly = j;
+        double answer;
+        long sum = 0;
+        int left = 0;
+        int right = 1;
+        while(right < list.size()) {
+            int[] first = list.get(left);
+            int[] second = list.get(right);
 
-                int rx = i;
-                int ry = j + 1;
+            int lx = first[0];
+            int ly = first[1];
+            int rx = second[0];
+            int ry = second[1];
+            sum += ((long)lx * ry) - ((long)rx * ly);
 
-                for(int k = 0; k < 2; k++) {
-                    int nlx = lx + dx[k];
-                    int nly = ly + dy[k];
-                    int nrx = rx + dx[k];
-                    int nry = ry + dy[k];
-
-                    if(isNotPossible(nlx, nly, k) || isNotPossible(nrx, nry, k)) {
-                        continue;
-                    }
-                    if(graph[lx][ly] == 'X' && graph[rx][ry] == 'X' && graph[nlx][nly] == '.' && graph[nrx][nry] == '.') {
-                        answer++;
-                        makeTrue(lx, ly, rx, ry, nlx, nly, nrx, nry, k);
-                    }
-                }
-            }
+            left++;
+            right++;
         }
-    }
-
-    private static void checkVertical() {
-        for(int i =0 ; i < M - 1; i++) {
-            for(int j = 0; j < N; j++) {
-                int ux = i;
-                int uy = j;
-
-                int bx = i + 1;
-                int by = j;
-
-                for(int k = 2; k < 4; k++) {
-                    int lux = ux + dx[k];
-                    int luy = uy + dy[k];
-                    int lbx = bx + dx[k];
-                    int lby = by + dy[k];
-
-                    if(isNotPossible(lux, luy, k) || isNotPossible(lbx, lby, k)) {
-                        continue;
-                    }
-
-                    if(graph[ux][uy] == 'X' && graph[bx][by] == 'X' && graph[lux][luy] == '.' && graph[lbx][lby] == '.') {
-                        answer++;
-                        makeTrue(ux, uy, bx, by, lux, luy, lbx, lby, k);
-                    }
-                }
-            }
-        }
-    }
-    private static void makeTrue(int x1, int y1, int x2, int y2, int x3, int y3, int x4, int y4, int k) {
-        visited[x1][y1][k] = true;
-        visited[x2][y2][k] = true;
-        visited[x3][y3][k] = true;
-        visited[x4][y4][k] = true;
-    }
-
-    private static boolean isNotPossible(int x, int y, int k) {
-        if(x < 0 || y < 0 || x >= M || y >= N) {
-            return true;
-        }
-        if(visited[x][y][k]) {
-            return true;
-        }
-        return false;
+        answer = Math.abs(sum) * 0.5;
+        System.out.printf("%.1f", answer);
     }
 }
