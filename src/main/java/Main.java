11218b77dfb964bda64b44b25c54905506235717
bingsuzoa <zxc_777@naver.com>
2025-12-08 07:39:12 +0900

    직사각형의 개수

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,44 +2,85 @@ import java.util.*;
 import java.io.*;
 
 class Main {
-    static List<int[]> list;
+    static Map<Long, Integer> map = new HashMap<>();
+    static List<int[]> list = new ArrayList<>();
+    static List<Integer>[] xList;
+    static List<Integer>[] yList;
+    static long answer = 0;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int N = Integer.parseInt(br.readLine());
-        list = new ArrayList<>();
 
-        for(int i =0 ; i < N; i++) {
+        if(N < 4) {
+            System.out.println(0);
+            return;
+        }
+
+        int idx = 0;
+        for(int i = 0; i < N; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
-            int x = Integer.parseInt(st.nextToken());
-            int y = Integer.parseInt(st.nextToken());
-            list.add(new int[]{x, y});
+            long x = Long.parseLong(st.nextToken());
+            long y = Long.parseLong(st.nextToken());
+            if(!map.containsKey(x)) {
+                map.put(x, idx++);
+            }
+            if(!map.containsKey(y)) {
+                map.put(y, idx++);
+            }
+            list.add(new int[]{map.get(x), map.get(y)});
         }
 
-        Collections.sort(list, (o1,o2) -> {
-            if(o1[0] == o2[0]) {
-                return o1[1] - o2[1];
-            }
-            return o1[0] - o2[0];
-        });
+        xList = new ArrayList[idx];
+        yList = new ArrayList[idx];
 
-        int[] first = list.get(0);
-        int start = first[0];
-        int end = first[1];
-        long result = (end - start);
-        for(int i = 1; i < list.size(); i++) {
-            int[] temp = list.get(i);
-            if(end < temp[0]) {
-                start = temp[0];
-                end = temp[1];
-                result += (end - start);
-            }
-            else if(start <= temp[0] && end < temp[1]) {
-                result += (temp[1] - end);
-                end = temp[1];
+        for(int i = 0; i < xList.length; i++) {
+            xList[i] = new ArrayList<>();
+            yList[i] = new ArrayList<>();
+        }
+
+        for(int[] temp : list) {
+            xList[temp[0]].add(temp[1]);
+            yList[temp[1]].add(temp[0]);
+        }
+
+        for(int i = 0; i < xList.length; i++) {
+            Collections.sort(xList[i]);
+            Collections.sort(yList[i]);
+        }
+
+        Set<String> uniqueSet = new HashSet<>();
+        for(int i = 0; i < xList.length; i++) {
+            List<Integer> temp = xList[i];
+            if(temp.size() < 2) continue;
+            for(int j = 0; j < temp.size(); j++) {
+                for(int k = j + 1; k < temp.size(); k++) {
+                    int left = temp.get(j);
+                    int right = temp.get(k);
+                    String value = String.valueOf(left) + " " + String.valueOf(right);
+                    if(!uniqueSet.contains(value)) {
+                        uniqueSet.add(value);
+                        check(yList[left], yList[right]);
+                    }
+                }
             }
         }
-        System.out.println(result);
+        System.out.println(answer);
+    }
+    private static void check(List<Integer> list1, List<Integer> list2) {
+        int count = 0;
+        for(int value : list1) {
+            if(list2.contains(value)) {
+                count++;
+            }
+        }
+        if(count == 0 || count == 1) return;
+
+        long sum = 0;
+        for(int i = count  - 1; i >= 1; i--) {
+            sum += i;
+        }
+        answer += sum;
     }
 }
