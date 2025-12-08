a2c8b7bed69f40478c0077faa8b89129c0b9628e
bingsuzoa <zxc_777@naver.com>
2025-12-09 07:29:57 +0900

    점 고르기

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,85 +2,42 @@ import java.util.*;
 import java.io.*;
 
 class Main {
-    static Map<Long, Integer> map = new HashMap<>();
+    static int N, A, B;
     static List<int[]> list = new ArrayList<>();
-    static List<Integer>[] xList;
-    static List<Integer>[] yList;
-    static long answer = 0;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int N = Integer.parseInt(br.readLine());
+        StringTokenizer input = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(input.nextToken());
+        A = Integer.parseInt(input.nextToken());
+        B = Integer.parseInt(input.nextToken());
 
-        if(N < 4) {
-            System.out.println(0);
-            return;
-        }
-
-        int idx = 0;
         for(int i = 0; i < N; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
-            long x = Long.parseLong(st.nextToken());
-            long y = Long.parseLong(st.nextToken());
-            if(!map.containsKey(x)) {
-                map.put(x, idx++);
-            }
-            if(!map.containsKey(y)) {
-                map.put(y, idx++);
-            }
-            list.add(new int[]{map.get(x), map.get(y)});
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+            int z = Integer.parseInt(st.nextToken());
+            list.add(new int[]{x, y, z});
         }
 
-        xList = new ArrayList[idx];
-        yList = new ArrayList[idx];
+        int max = 0;
+        for(int i = 0; i < list.size(); i++) {
+            for(int j = 0; j < list.size(); j++) {
+                if(i == j) continue;
+                int[] array = list.get(i);
+                int sx = array[0];
+                int sy = array[1];
 
-        for(int i = 0; i < xList.length; i++) {
-            xList[i] = new ArrayList<>();
-            yList[i] = new ArrayList<>();
-        }
+                int[] pair = list.get(j);
+                int ex = pair[0];
+                int ey = pair[1];
 
-        for(int[] temp : list) {
-            xList[temp[0]].add(temp[1]);
-            yList[temp[1]].add(temp[0]);
-        }
-
-        for(int i = 0; i < xList.length; i++) {
-            Collections.sort(xList[i]);
-            Collections.sort(yList[i]);
-        }
-
-        Set<String> uniqueSet = new HashSet<>();
-        for(int i = 0; i < xList.length; i++) {
-            List<Integer> temp = xList[i];
-            if(temp.size() < 2) continue;
-            for(int j = 0; j < temp.size(); j++) {
-                for(int k = j + 1; k < temp.size(); k++) {
-                    int left = temp.get(j);
-                    int right = temp.get(k);
-                    String value = String.valueOf(left) + " " + String.valueOf(right);
-                    if(!uniqueSet.contains(value)) {
-                        uniqueSet.add(value);
-                        check(yList[left], yList[right]);
-                    }
+                if(Math.abs(ex - sx) + 1 <= A && Math.abs(sy - ey)  + 1 <= B) {
+                    max = Math.max(Math.abs(array[2] - pair[2]), max);
                 }
             }
         }
-        System.out.println(answer);
-    }
-    private static void check(List<Integer> list1, List<Integer> list2) {
-        int count = 0;
-        for(int value : list1) {
-            if(list2.contains(value)) {
-                count++;
-            }
-        }
-        if(count == 0 || count == 1) return;
-
-        long sum = 0;
-        for(int i = count  - 1; i >= 1; i--) {
-            sum += i;
-        }
-        answer += sum;
+        System.out.println(max);
     }
 }
