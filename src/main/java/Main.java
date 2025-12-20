6120890c6b217d0d07f46668aadafa1b2559e6e3
bingsuzoa <zxc_777@naver.com>
2025-12-20 11:41:10 +0900

    수고르기

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,38 +2,36 @@ import java.util.*;
 import java.io.*;
 
 class Main {
+    static int N;
+    static long M;
+    static long[] graph;
+    static long answer = Long.MAX_VALUE;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int N = Integer.parseInt(br.readLine());
-
-        int[] graph = new int[N];
-        int[] dp = new int[N];
-
         StringTokenizer st = new StringTokenizer(br.readLine());
-        for(int i = 0; i < N; i++){
-            graph[i] = Integer.parseInt(st.nextToken());
-        }
 
-        for(int i = 1; i < graph.length; i++){
-            for(int j = 0; j <= i; j++) {
-                int min = Integer.MAX_VALUE;
-                int max = -1;
-                for(int k = j; k <= i; k++) {
-                    min = Math.min(graph[k], min);
-                    max = Math.max(graph[k], max);
-                }
-                if(j == 0) {
-                    dp[i] = Math.max(dp[i], max - min);
-                } else {
-                    dp[i] = Math.max(dp[i], dp[j-1] + max -min);
-                }
+        N = Integer.parseInt(st.nextToken());
+        M = Long.parseLong(st.nextToken());
+        graph = new long[N];
+
+        for(int i = 0; i < graph.length; i++) {
+            graph[i] = Long.parseLong(br.readLine());
+        }
+        Arrays.sort(graph);
+
+        int left = 0;
+        int right = 1;
+
+        while(right < graph.length && left < graph.length) {
+            if(graph[right] - graph[left] < M) {
+                right++;
+            } else {
+                answer = Math.min(answer, graph[right] - graph[left]);
+                left++;
             }
         }
-        for(int value : dp) {
-            System.out.print(value + " ");
-        }
-        System.out.println(dp[dp.length-1]);
+        System.out.println(answer);
     }
 }
