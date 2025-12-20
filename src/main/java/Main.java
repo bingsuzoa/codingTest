ba33556bf985abb61476f80ce6d6bdd0f1819e20
bingsuzoa <zxc_777@naver.com>
2025-12-20 10:08:41 +0900

    조짜기

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,33 +2,38 @@ import java.util.*;
 import java.io.*;
 
 class Main {
-    static int MOD = 1000000000;
-    static long[][] dp;
-
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
+        int N = Integer.parseInt(br.readLine());
+
+        int[] graph = new int[N];
+        int[] dp = new int[N];
+
         StringTokenizer st = new StringTokenizer(br.readLine());
-
-        int N = Integer.parseInt(st.nextToken());
-        int K = Integer.parseInt(st.nextToken());
-
-        dp = new long[K + 1][N + 1];
-
-        for(int i = 1; i <= K; i++) {
-            dp[i][0] = 1;
-        }
-        for(int i = 1; i <= N; i++) {
-            dp[1][i] = 1;
+        for(int i = 0; i < N; i++){
+            graph[i] = Integer.parseInt(st.nextToken());
         }
 
-        for(int i = 2; i < dp.length; i++) {
-            for(int j = 1; j < dp[i].length; j++) {
-                dp[i][j] = (dp[i][j-1] + dp[i-1][j]) % MOD;
+        for(int i = 1; i < graph.length; i++){
+            for(int j = 0; j <= i; j++) {
+                int min = Integer.MAX_VALUE;
+                int max = -1;
+                for(int k = j; k <= i; k++) {
+                    min = Math.min(graph[k], min);
+                    max = Math.max(graph[k], max);
+                }
+                if(j == 0) {
+                    dp[i] = Math.max(dp[i], max - min);
+                } else {
+                    dp[i] = Math.max(dp[i], dp[j-1] + max -min);
+                }
             }
         }
-
-        System.out.println(dp[K][N] % MOD);
+        for(int value : dp) {
+            System.out.print(value + " ");
+        }
+        System.out.println(dp[dp.length-1]);
     }
 }
