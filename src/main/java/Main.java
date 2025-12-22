86d0a07f93db7de28cef35fb7a0819396b02d1c4
bingsuzoa <zxc_777@naver.com>
2025-12-23 07:25:12 +0900

    극장 좌석

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,18 +2,49 @@ import java.util.*;
 import java.io.*;
 
 class Main {
-    static int MOD = 1000000;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        long N = Long.parseLong(br.readLine());
+        int N = Integer.parseInt(br.readLine());
+        int M = Integer.parseInt(br.readLine());
 
-        long sum = 0;
-        for(long i = 2; i <= N / 2; i++) {
-            long count = N / i - 1;
-            sum += (i * count) % MOD;
+        int[] graph = new int[M];
+        for(int i = 0; i < graph.length; i++) {
+            graph[i] = Integer.parseInt(br.readLine());
         }
-        System.out.println(sum % MOD);
+
+        if(N == 1) {
+            System.out.println(1);
+            return;
+        }
+
+        int[] dp = new int[N+1];
+        dp[1] = 1;
+        dp[2] = 2;
+
+        for(int i = 3; i < dp.length; i++) {
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+
+        List<Integer> list = new ArrayList<>();
+        int people = 0;
+        for(int value : graph) {
+            if(value - people -1 == 0) {
+                people = value;
+                continue;
+            }
+            list.add(value - people - 1);
+            people = value;
+        }
+        if(people < N) {
+            list.add(N - people);
+        }
+
+        int sum = 1;
+        for(int result : list) {
+            sum *= dp[result];
+        }
+        System.out.println(sum);
     }
 }
