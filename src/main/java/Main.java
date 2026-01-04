7e66f65c3603fd48f5a924206941cdfe6a405759
bingsuzoa <zxc_777@naver.com>
2026-01-05 08:29:41 +0900

    문자열 암호화

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,70 +1,51 @@
 import java.util.*;
 import java.io.*;
 
+
 class Main {
-    static int N,M;
-    static int[][] graph;
-    static int X;
-    static int pos;
-    static boolean[][] visited;
-    static Queue<int[]> queue;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        N = Integer.parseInt(br.readLine());
-        M = Integer.parseInt(br.readLine());
-        graph = new int[N][M];
+        while(true) {
+            int N = Integer.parseInt(br.readLine());
+            if(N == 0) break;
+            String input = br.readLine().toUpperCase().replace(" ", "");
 
-        for(int i = 0; i < graph.length; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            for(int j = 0; j < graph[i].length; j++) {
-                graph[i][j] = Integer.parseInt(st.nextToken());
-            }
-        }
+            char[] graph = new char[input.length()];
+            Arrays.fill(graph, '0');
+            graph[0] = input.charAt(0);
 
-        X = Integer.parseInt(br.readLine());
 
-        visited = new boolean[N][M];
-        queue = new LinkedList<>();
-        pos = graph[0][0];
-        queue.add(new int[]{0,0});
-        visited[0][0] = true;
-
-        if(graph[N-1][M-1] != pos) {
-            System.out.println("DEAD");
-            return;
-        }
-
-        while(!queue.isEmpty()) {
-            int[] cur = queue.poll();
-            int sx = cur[0];
-            int sy = cur[1];
-
-            if(sx == N-1 && sy == M-1) {
-                System.out.println("ALIVE");
-                return;
-            }
-
-            getMan(sx, sy);
-        }
-
-        System.out.println("DEAD");
-
-    }
-    private static void getMan(int sx, int sy) {
-        for(int i = graph.length - 1; i >= 0; i--) {
-            for(int j = graph[i].length -1; j >= 0; j--) {
-                if(isMan(sx, sy, i, j) && graph[i][j] == pos && !visited[i][j]) {
-                    visited[i][j] = true;
-                    queue.add(new int[]{i, j});
+            int cur = 0;
+            for(int i = 1; i < input.length(); i++) {
+                char c = input.charAt(i);
+                cur += N;
+                if(cur >= graph.length) {
+                    cur = getBlankIdx(graph);
+                    if(cur == -1) {
+                        break;
+                    }
                 }
+                graph[cur] = c;
             }
+            print(graph);
         }
     }
+    private static void print(char[] graph) {
+        StringBuilder sb = new StringBuilder();
+        for(char c : graph) {
+            sb.append(c);
+        }
+        System.out.println(sb);
+    }
 
-    private static boolean isMan(int sx, int sy, int ex, int ey) {
-        int length = Math.abs(sx - ex) + Math.abs(sy - ey);
-        return length <= X;
+    private static int getBlankIdx(char[] graph) {
+        for(int i = 0; i < graph.length; i++) {
+            if(graph[i] == '0') {
+                return i;
+            }
+        }
+        return -1;
     }
 }
