6d915a7c5e0eafe43f04c965156593cc8783a270
bingsuzoa <zxc_777@naver.com>
2026-01-04 11:11:53 +0900

    10719

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,33 +2,61 @@ import java.util.*;
 import java.io.*;
 
 class Main {
-    static int N, K;
+    static int[] costs;
     static int[] graph;
-
+    static int[][] dp;
+    static int n,m,c;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
 
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        N = Integer.parseInt(st.nextToken());
-        K = Integer.parseInt(st.nextToken());
+        int T = Integer.parseInt(br.readLine());
 
-        graph = new int[K];
-        for(int i = 0; i < graph.length; i++) {
-            graph[i] = Integer.parseInt(br.readLine());
+        for(int i = 0; i < T; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            n = Integer.parseInt(st.nextToken());
+            m = Integer.parseInt(st.nextToken());
+            c = Integer.parseInt(st.nextToken());
+
+            costs = new int[m+1];
+            StringTokenizer st1 = new StringTokenizer(br.readLine());
+            for(int j = 1; j <= m; j++) {
+                costs[j] = Integer.parseInt(st1.nextToken());
+            }
+
+            graph = new int[n];
+            StringTokenizer st2 = new StringTokenizer(br.readLine());
+            for(int j = 0; j < graph.length; j++) {
+                graph[j] = Integer.parseInt(st2.nextToken());
+            }
+
+            dp = new int[n][m+1];
+            for(int j = 0; j < dp.length; j++) {
+                Arrays.fill(dp[j], 1_000_000_000);
+            }
+
+            sb.append(dfs(0,0)).append("\n");
         }
-        Arrays.sort(graph);
 
-        int re1 = graph[0];
-        int re2 = N - graph[K-1];
-
-        int answer = Math.max(re1, re2);
-
-        for(int i = 0; i < graph.length - 1; i++) {
-            answer = Math.max(answer, (graph[i+1] - graph[i]) / 2);
-        }
-        System.out.println(answer);
+        System.out.println(sb.toString().trim());
     }
 
+    private static int dfs(int cur, int save) {
+        if(cur == n) {
+            return 0;
+        }
 
+        if(dp[cur][save] != 1_000_000_000) return dp[cur][save];
+
+        int ref = 1_000_000_000;
+        if(save == graph[cur]) {
+            ref = Math.min(ref, dfs(cur + 1, save));
+        } else {
+            // area1에 안옮겨
+            ref = Math.min(ref, dfs(cur + 1, save) + costs[graph[cur]]);
+            ref = Math.min(ref, dfs(cur + 1, graph[cur]) + c);
+        }
+        return dp[cur][save] = Math.min(dp[cur][save], ref);
+    }
 }
