c7a7f99061bd0ca2473a6562c9c0dbbcd83a994c
bingsuzoa <zxc_777@naver.com>
2026-01-13 08:01:08 +0900

    fraction

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,43 +2,95 @@ import java.util.*;
 import java.io.*;
 
 class Main {
-    static String st1, st2;
-    static int n, m;
-    static char[] array1;
-    static char[] array2;
-    static int[][] dp;
+    static char[] graph;
+    static Stack<long[]> stack;
 
-    public static void main(String[] args) throws IOException {
-
+    public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        st1 = br.readLine();
-        st2 = br.readLine();
+        int n = Integer.parseInt(br.readLine());
+        graph = new char[n];
 
-        n = st1.length();
-        m = st2.length();
-
-        dp = new int[n+1][m+1];
-        array1 = new char[n];
-        array2 = new char[m];
-
-        for(int i = 0; i < st1.length(); i++) {
-            array1[i] = st1.charAt(i);
-        }
-        for(int i = 0; i < st2.length(); i++) {
-            array2[i] = st2.charAt(i);
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for(int i =0; i < graph.length; i++) {
+            graph[i] = st.nextToken().charAt(0);
         }
 
-        for(int i = 1; i < dp.length; i++) {
-            for(int j = 1; j < dp[0].length; j++) {
-                if(array1[i-1] == array2[j-1]) {
-                    dp[i][j] = dp[i-1][j-1] + 1;
-                } else {
-                    dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1]);
-                }
+        if(!validate()) {
+            System.out.println(-1);
+            return;
+        }
+        stack = new Stack<>();
+
+        for(int i = 0; i < graph.length; i++) {
+            char c = graph[i];
+            if(c == ')') {
+                calculate();
+            }
+            if(isDigit(c)) {
+                stack.push(new long[]{c - '0', 1});
             }
         }
 
-        System.out.println(dp[n][m]);
+        if(stack.size() != 1) {
+            System.out.println(-1);
+            return;
+        }
+        long[] result = stack.pop();
+        System.out.println(result[0] + " " + result[1]);
+    }
+    private static void calculate() {
+        if(stack.size() >= 3) {
+            long[] n1 = stack.pop();
+            long e = n1[0];
+            long f = n1[1];
+            long[] n2 = stack.pop();
+            long c = n2[0];
+            long d = n2[1];
+            long[] n3 = stack.pop();
+            long a = n3[0];
+            long b = n3[1];
+
+            long upper = (b * c * f) + (a * d * e);
+            long bottom = b * d * e;
+
+            long gcd = gcd(upper, bottom);
+            stack.push(new long[]{upper / gcd, bottom / gcd});
+        }
+    }
+    private static long gcd(long a, long b) {
+        while(b != 0) {
+            long tmp = a % b;
+            a = b;
+            b = tmp;
+        }
+        return a;
+    }
+
+    private static boolean isDigit(char c) {
+        if(c - '0' >= 0 && c - '0' <= 9) {
+            return true;
+        }
+        return false;
+    }
+
+    private static boolean validate() {
+        Stack stack = new Stack<>();
+        for(char c : graph) {
+            if(c == '(') {
+                stack.add('(');
+            }
+            else if(c == ')') {
+                if(stack.isEmpty()) {
+                    return false;
+                }
+                stack.pop();
+            }
+        }
+
+        if(!stack.isEmpty()) {
+            return false;
+        }
+        return true;
     }
 }
