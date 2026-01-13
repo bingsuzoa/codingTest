9886400d90c0c1b85f544408f115d697c7768ebf
bingsuzoa <zxc_777@naver.com>
2026-01-14 08:07:05 +0900

    고독

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,95 +2,30 @@ import java.util.*;
 import java.io.*;
 
 class Main {
-    static char[] graph;
-    static Stack<long[]> stack;
+    static int[] graph;
 
-    public static void main(String[] args) throws IOException{
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int n = Integer.parseInt(br.readLine());
-        graph = new char[n];
-
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        for(int i =0; i < graph.length; i++) {
-            graph[i] = st.nextToken().charAt(0);
-        }
-
-        if(!validate()) {
-            System.out.println(-1);
-            return;
-        }
-        stack = new Stack<>();
+        int N = Integer.parseInt(br.readLine());
+        graph = new int[N];
 
         for(int i = 0; i < graph.length; i++) {
-            char c = graph[i];
-            if(c == ')') {
-                calculate();
-            }
-            if(isDigit(c)) {
-                stack.push(new long[]{c - '0', 1});
-            }
+            graph[i] = Integer.parseInt(br.readLine());
         }
+        Arrays.sort(graph);
 
-        if(stack.size() != 1) {
-            System.out.println(-1);
-            return;
-        }
-        long[] result = stack.pop();
-        System.out.println(result[0] + " " + result[1]);
-    }
-    private static void calculate() {
-        if(stack.size() >= 3) {
-            long[] n1 = stack.pop();
-            long e = n1[0];
-            long f = n1[1];
-            long[] n2 = stack.pop();
-            long c = n2[0];
-            long d = n2[1];
-            long[] n3 = stack.pop();
-            long a = n3[0];
-            long b = n3[1];
+        long count = graph[0] - 1;
+        graph[0] = 1;
 
-            long upper = (b * c * f) + (a * d * e);
-            long bottom = b * d * e;
+        for(int i = 1; i < graph.length; i++) {
+            int next = graph[i-1] + 1;
 
-            long gcd = gcd(upper, bottom);
-            stack.push(new long[]{upper / gcd, bottom / gcd});
-        }
-    }
-    private static long gcd(long a, long b) {
-        while(b != 0) {
-            long tmp = a % b;
-            a = b;
-            b = tmp;
-        }
-        return a;
-    }
-
-    private static boolean isDigit(char c) {
-        if(c - '0' >= 0 && c - '0' <= 9) {
-            return true;
-        }
-        return false;
-    }
-
-    private static boolean validate() {
-        Stack stack = new Stack<>();
-        for(char c : graph) {
-            if(c == '(') {
-                stack.add('(');
-            }
-            else if(c == ')') {
-                if(stack.isEmpty()) {
-                    return false;
-                }
-                stack.pop();
+            if(graph[i] >= next) {
+                count += graph[i] - next;
+                graph[i] = next;
             }
         }
-
-        if(!stack.isEmpty()) {
-            return false;
-        }
-        return true;
+        System.out.println(count);
     }
 }
