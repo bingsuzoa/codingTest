891aa830ec77f02792a4002f3fb435125d9a71b6
bingsuzoa <zxc_777@naver.com>
2026-01-15 08:09:28 +0900

    20260112

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,31 +1,44 @@
 import java.util.*;
 import java.io.*;
 
+
 class Main {
-    static int[] graph;
+    static Set<Integer> set = new HashSet<>();
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int N = Integer.parseInt(br.readLine());
-        graph = new int[N];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
 
-        for(int i = 0; i < graph.length; i++) {
-            graph[i] = Integer.parseInt(br.readLine());
-        }
-        Arrays.sort(graph);
-
-        long count = graph[0] - 1;
-        graph[0] = 1;
-
-        for(int i = 1; i < graph.length; i++) {
-            int next = graph[i-1] + 1;
-
-            if(graph[i] >= next) {
-                count += graph[i] - next;
-                graph[i] = next;
-            }
+        int count = 0;
+        while(A < B) {
+            count += isCheck(A, B);
+            A++;
         }
         System.out.println(count);
+    }
+
+    private static int isCheck(int x, int B) {
+        String s = String.valueOf(x);
+        int len = s.length();
+
+        set.clear();
+
+        for(int i = len -1; i > 0; i--) {
+            StringBuilder sb = new StringBuilder();
+            for(int j = i; j < len; j++) {
+                sb.append(s.charAt(j));
+            }
+            for(int j = 0; j < i; j++) {
+                sb.append(s.charAt(j));
+            }
+            int y = Integer.parseInt(sb.toString());
+            if(x < y && y <= B) {
+                set.add(y);
+            }
+        }
+        return set.size();
     }
 }
