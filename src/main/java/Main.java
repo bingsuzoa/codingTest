02598c00a930d5d051233286408f5e6e5f848dbc
bingsuzoa <zxc_777@naver.com>
2026-01-16 08:51:35 +0900

    20260116

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,44 +1,56 @@
 import java.util.*;
 import java.io.*;
 
-
 class Main {
-    static Set<Integer> set = new HashSet<>();
+    static String input;
+    static int N;
+    static Map<Character, Integer> map;
+    static int size;
+    static int[][] dp;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int A = Integer.parseInt(st.nextToken());
-        int B = Integer.parseInt(st.nextToken());
+        N = Integer.parseInt(br.readLine());
 
-        int count = 0;
-        while(A < B) {
-            count += isCheck(A, B);
-            A++;
+        input = br.readLine();
+        size = input.length();
+
+        map = new HashMap<>();
+        map.put('B', 0);
+        map.put('L', 1);
+        map.put('D', 2);
+
+        dp = new int[size][size];
+        for(int i = 0; i < dp.length; i++) {
+            for(int j = 0; j < dp[i].length; j++) {
+                Arrays.fill(dp[i], -1);
+            }
         }
-        System.out.println(count);
+
+        System.out.println(dfs(0, size -1, 0));
     }
 
-    private static int isCheck(int x, int B) {
-        String s = String.valueOf(x);
-        int len = s.length();
-
-        set.clear();
-
-        for(int i = len -1; i > 0; i--) {
-            StringBuilder sb = new StringBuilder();
-            for(int j = i; j < len; j++) {
-                sb.append(s.charAt(j));
-            }
-            for(int j = 0; j < i; j++) {
-                sb.append(s.charAt(j));
-            }
-            int y = Integer.parseInt(sb.toString());
-            if(x < y && y <= B) {
-                set.add(y);
-            }
+    private static int dfs(int left, int right, int need) {
+        if(left > right) {
+            return 0;
         }
-        return set.size();
+        if(dp[left][right] != -1) {
+            return dp[left][right];
+        }
+
+        int curL = map.get(input.charAt(left));
+        int curR = map.get(input.charAt(right));
+        int next = (need + 1) % 3;
+
+        dp[left][right] = 0;
+
+        if(curL == need) {
+            dp[left][right] = Math.max(dp[left][right], dfs(left + 1, right, next) + 1);
+        }
+        if(curR == need) {
+            dp[left][right] = Math.max(dp[left][right], dfs(left, right -1, next) + 1);
+        }
+        return dp[left][right];
     }
 }
